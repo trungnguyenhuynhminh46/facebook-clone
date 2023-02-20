0b1a270e6805a1133778c64b989ecb1b9a528e9f
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2023-02-20 20:14:58 +0700

    assets prepare

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ArrowDown, HomeActive } from "./svg";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <div className="flex gap-10 items-center">
+        <span>Hello world!</span>
+        <ArrowDown color="red" />
+        <HomeActive color="red" />
+        <i className="friends_requests_icon"></i>
+      </div>
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/svg/dots.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface Props {
+  color: string;
+}
+const Dots: React.FC<Props> = ({ color }) => {
+  return (
+    <svg width="20" height="20" fill={color} viewBox="0 0 20 20">
+      <path
+        fillRule="evenodd"
+        d="M458 360a2 2 0 11-4 0 2 2 0 014 0m6 0a2 2 0 11-4 0 2 2 0 014 0m-12 0a2 2 0 11-4 0 2 2 0 014 0"
+        transform="translate(-446 -350)"
+      ></path>
+    </svg>
+  );
+};
+
+export default Dots;
